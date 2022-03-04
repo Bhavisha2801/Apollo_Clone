a9de0ce14ecc344cbcc1cb12b11b6b095d0419d7
Luckyyadav420 <97690042+Luckyyadav420@users.noreply.github.com>
2022-03-04 19:07:08 +0530

    navbar before login

---
+++ navbar_before_login.js
@@ -0,0 +1,26 @@
+function navbar() {
+    return `<div class="fixed_header">
+    <div class="navbar">
+        <div class="logo">
+            <img src="https://newassets.apollo247.com/images/ic_logo.png" alt="logo">
+        </div>
+        <div class="options">
+            <p><a href="Doctors.html">Doctors</a> <br> <span class="line"></span> <a>Consult <br> Online</a>
+            </p>
+            <p><a href="Pharmacy.html">Pharmacy</a> <br><span class="line"></span> <a>Medicnes & <br> other
+                    products</a> </p>
+            <p><a href="Lab_Tests.html">Lab Tests</a> <br> <span class="line"></span> <a> Health
+                    <br>check-ups</a> </p>
+            <p><a href="Covid_19.html">Covid 19</a> <br> <span class="line"></span> <a>Latest <br>Updates</a>
+            </p>
+            <img id="cart.png" alt="">
+            <img class="user-profile" src="user-profilee.webp" alt="user-profile">
+
+
+         </div>
+        </div>
+    </div>`;
+    
+}
+
+export default navbar;
