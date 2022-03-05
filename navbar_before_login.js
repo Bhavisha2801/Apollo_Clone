b86d7a0751e0fc6d7a0b33a8a733db18f4e6ce21
Bhavisha2801 <nayibhavisha@gmail.com>
2022-03-06 03:04:56 +0530

    created pharmacy page

--- navbar_before_login.js
+++ navbar_before_login.js
@@ -7,11 +7,11 @@ function navbar() {
         <div class="options">
             <p><a href="Doctors.html">Doctors</a> <br> <span class="line"></span> <a>Consult <br> Online</a>
             </p>
-            <p><a href="Pharmacy.html">Pharmacy</a> <br><span class="line"></span> <a>Medicnes & <br> other
+            <p><a href="pharmacyy.html">Pharmacy</a> <br><span class="line"></span> <a>Medicnes & <br> other
                     products</a> </p>
             <p><a href="Lab_Tests.html">Lab Tests</a> <br> <span class="line"></span> <a> Health
                     <br>check-ups</a> </p>
-            <p><a href="Blog.html">Blog/a> <br> <span class="line"></span> <a>Latest <br>Updates</a>
+            <p><a href="Blog.html">Blog</a> <br> <span class="line"></span> <a>Latest <br>Updates</a>
             </p>
             <img id="cart.png" alt="">
             <a onclick="openForm()" id="anchor">
