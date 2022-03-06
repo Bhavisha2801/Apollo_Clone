80fff299b7e8415fdf3e821fce44c8d34d39ce53
Luckyyadav420 <97690042+Luckyyadav420@users.noreply.github.com>
2022-03-06 18:57:28 +0530

    new updated code

--- navbar_before_login.js
+++ navbar_before_login.js
@@ -2,19 +2,22 @@ function navbar() {
     return `<div class="fixed_header">
     <div class="navbar">
         <div class="logo">
-            <img src="https://newassets.apollo247.com/images/ic_logo.png" alt="logo">
+        <a href="./apolo.html"><img src="https://newassets.apollo247.com/images/ic_logo.png" alt="logo"></a>
+        
         </div>
         <div class="options">
-            <p><a href="Doctors.html">Doctors</a> <br> <span class="line"></span> <a>Consult <br> Online</a>
+            <p><a href="Doctors.html">Doctors</a> <br> <span class="line" id ="line1"></span> <a>Consult <br> Online</a>
             </p>
-            <p><a href="pharmacyy.html">Pharmacy</a> <br><span class="line"></span> <a>Medicnes & <br> other
+            <p><a href="pharmacyy.html">Pharmacy</a> <br><span class="line" id ="line2"></span> <a>Medicnes & <br> other
                     products</a> </p>
-            <p><a href="health_record.html">Health Record</a> <br> <span class="line"></span> <a> Health
-                    <br>check-ups</a> </p>
-            <p><a href="Blog.html">Blog</a> <br> <span class="line"></span> <a>Latest <br>Updates</a>
-            <p><a href="Covid_19.html">Covid 19</a> <br> <span class="line"></span> <a>Latest <br>Updates</a>
+            <p><a href="appointment.html">Appointment</a> <br> <span class="line" id ="line3"></span> <a> Health
+                    <br>check-ups</a> </p> 
+            <p><a href="Blog.html">Blog</a> <br> <span class="line" id ="line4"></span> <a>Latest <br>Updates</a>
+            
             </p>
-            <img class="cart" src="cart.png" alt="">
+            <p><a href="health_record.html"> Health Record</a> <span class="line" id ="line5"></span> Health <br>Records</p>
+            <a href="cart.html"><img class="cart" src="cart.png" alt=""></a>
+            
             <a onclick="openForm()"><img class="user-profile" src="./opoloimages/user-profilee.webp" alt="user-profile"></a>
 
 
