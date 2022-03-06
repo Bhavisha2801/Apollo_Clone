5aae06921d6b96249060a892bb56ad5a27486bce
Luckyyadav420 <97690042+Luckyyadav420@users.noreply.github.com>
2022-03-06 15:01:51 +0530

    navbar update

--- navbar_before_login.js
+++ navbar_before_login.js
@@ -6,16 +6,16 @@ function navbar() {
         
         </div>
         <div class="options">
-            <p><a href="Doctors.html">Doctors</a> <br> <span class="line"></span> <a>Consult <br> Online</a>
+            <p><a href="Doctors.html">Doctors</a> <br> <span class="line" id ="line1"></span> <a>Consult <br> Online</a>
             </p>
-            <p><a href="pharmacyy.html">Pharmacy</a> <br><span class="line"></span> <a>Medicnes & <br> other
+            <p><a href="pharmacyy.html">Pharmacy</a> <br><span class="line" id ="line2"></span> <a>Medicnes & <br> other
                     products</a> </p>
-            <p><a href="appointment.html">Appointment</a> <br> <span class="line"></span> <a> Health
-                    <br>check-ups</a> </p>
-            <p><a href="Blog.html">Blog</a> <br> <span class="line"></span> <a>Latest <br>Updates</a>
+            <p><a href="appointment.html">Appointment</a> <br> <span class="line" id ="line3"></span> <a> Health
+                    <br>check-ups</a> </p> 
+            <p><a href="Blog.html">Blog</a> <br> <span class="line" id ="line4"></span> <a>Latest <br>Updates</a>
             
             </p>
-            <p><a href="health_record.html"> Health Record</a> <span class="line"></span> Health <br>Records</p>
+            <p><a href="health_record.html"> Health Record</a> <span class="line" id ="line5"></span> Health <br>Records</p>
             <a href="cart.html"><img class="cart" src="cart.png" alt=""></a>
             
             <a onclick="openForm()"><img class="user-profile" src="./opoloimages/user-profilee.webp" alt="user-profile"></a>
